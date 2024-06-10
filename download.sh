690c4c8875f2df20f6d30d19114ace5a990a64de
wrathinmind <wrathinmind@gmail.com>
2024-06-10 19:41:55 +0300

    fix

--- download.sh
+++ download.sh
@@ -5,10 +5,11 @@ in=$1
 out=$2
 echo $in
 r=$(curl $in -s | egrep -o '/redirect/[0-9]+' | head -n 1)
-echo $r
+echo "Redirect: $r "
 if [ -z $r ]; then echo "episode not found, exiting"; exit; fi
 link=$(curl -L -s https://s.to/$r | egrep -o  "hls'.*'" | tail -c+8 | tr -d "'")
-echo $link
+link=$(echo $link | base64 -d)
+echo "Link: $link"
 echo ffmpeg -user_agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" -i $link -c copy $out
 ffmpeg -user_agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" -i $link -c copy -y $out
 
